9a9c3987221d7c7be4e02f8d056d3f6f3bd6ee54
Greg Buckner <gbuckner23@gmail.com>
2013-09-30 11:45:37 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Scholasphere::Application.config.secret_key_base = 'ca5a138da3fbd873ac6c142b9baa439f2cd6c6fa0bc80c605b1998fbc8f3db13e8e76ee4fd6a401afc2755ac02845aeaab3a95af7ad34973a48b2d9ca63894f2'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Scholasphere::Application.routes.draw do
+ 
+ get 'one-page-wonder' => redirect('/one-page-wonder/one-page-wonder.html')
+
+end
